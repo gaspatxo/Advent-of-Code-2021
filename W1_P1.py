b56226b713bcaa3a18360b2032ad12798a9a5b80
gaspatxo <83580019+gaspatxo@users.noreply.github.com>
2021-12-07 19:19:13 +0100

    Update W1_P1.py

--- W1_P1.py
+++ W1_P1.py
@@ -1,6 +1,6 @@
 increase_counter = 0
 
-with open('W1_P1_input.txt', 'r') as f:
+with open('D1_P1_input.txt', 'r') as f:
     clean_list = [int(line) for line in f.readlines()]
 
 for i in range(len(clean_list)-1):
